65c11a1bd37f6bd134c36ba82a2bb98982ab3b76
Денис Браусов <brausowdenis@gmail.com>
2023-06-27 14:55:09 +0300

    fix model mapper

--- src/main/java/com/library/dto/BookDto.java
+++ src/main/java/com/library/dto/BookDto.java
@@ -2,9 +2,11 @@ package com.library.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookDto {
     private Long id;
     private String title;
